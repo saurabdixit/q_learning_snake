788287318a0f7111da9213a3bf420360118c8b86
saurabdixit <saurabh.dxt21@gmail.com>
2016-06-06 18:11:07 -0700

    some error fixed now working good

--- main.m
+++ main.m
@@ -4,20 +4,21 @@ clear
 
 %% Inputs for the game
 grid_size = 10;
-initial_snake_length = grid_size;
+initial_snake_length =5;
 sleep = 0.00;
 reward_for_fruit = 1000;
-reward_for_moving =-10;
+reward_for_moving =-3;
 reward_for_hitting = -1000;
-learning_rate_self = 0.9;
-discount_factor_self = 0.2;
-learning_rate = 0.5;
-discount_factor = 0.7;
+learning_rate_self = 0.3;
+discount_factor_self = 0.1;
+learning_rate = 0.9;
+discount_factor = 0.9;
 explore_exploit_threshold =0.5;
-fid = fopen('result.txt','a')
+fid = fopen('result.txt','a');
 
 %% code starts here
 world = zeros(grid_size,grid_size);
+previous_idx_merge = [];
 snake_location = [(grid_size-initial_snake_length)+[1:initial_snake_length]',ones(initial_snake_length,1),ones(initial_snake_length,1)];
 for j=1:size(snake_location,1)
     world(snake_location(j,1),snake_location(j,2)) = 1;
@@ -43,13 +44,19 @@ action=100;
 previous_action_taken = 1;
 
 iterator = 1;
+snake_length_vec = [];
+tic;
+count = 0;
+visualize_world(world,snake_location,fruit_r,fruit_c,sleep,iterator);
 while 1
     fruit_eaten = 0;
     %action = input('Enter a action from 1-4: ');%randsample([1,2,3,4]);
     snake_length = size(snake_location,1);
 
-    valid_action  = 0;
-    testing_phase = rem(iterator,100) ==0;
+    testing_phase = rem(iterator,30) == 0;
+	if testing_phase && count == 0
+		count =10
+	end
     vec_action = [1,2,3,4];
     invalid_action = get_invalid_action(snake_location,grid_size);
     vec_action = vec_action(find(vec_action ~= invalid_action));
@@ -58,13 +65,19 @@ while 1
         ind_1 = sub2ind(size(world),snake_location(1,1),snake_location(1,2));
         [~,action_id] = max(Global_Q_matrix(ind_1,vec_action));
         action = vec_action(action_id);
-        if testing_phase
-            fprintf('Action taken: %d , invalid: %d \n',action,invalid_action);
-        end
+        %if testing_phase
+        %    %fprintf('Action taken: %d , invalid: %d \n',action,invalid_action);
+        %end
     else
         action = randsample(vec_action,1);
     end
+	time = toc;
+	if ~fruit_eaten && time > 5
+        action = randsample(vec_action,1);
+		tic;
+	end
 
+	
     %if action == 1
     %    disp('UP')
     %elseif action ==2
@@ -85,11 +98,14 @@ while 1
     ind_in_fruit_previous = find(Q_matrix_fruit(:,3) == ind_previous);
     ind_in_fruit_current = find(Q_matrix_fruit(:,3) == ind_current);
     [ind_in_fruit_current, ind_in_fruit_previous, action];
+	%Global_Q_matrix = zeros(grid_size * grid_size, 4);
     if sum(sum(world==1.5)) == 1
         fruit_eaten = 1;
-        %if ~testing_phase
-            Q_matrix_fruit(ind_in_fruit_previous,3+action) = Q_matrix_fruit(ind_in_fruit_previous,3+action) + learning_rate * (reward_for_fruit - Q_matrix_fruit(ind_in_fruit_previous,3+action));
-        %end
+		tic;
+		%Q_matrix_fruit
+        if ~testing_phase
+			Q_matrix_fruit(ind_in_fruit_previous,3+action) = Q_matrix_fruit(ind_in_fruit_previous,3+action) + learning_rate * (reward_for_fruit - Q_matrix_fruit(ind_in_fruit_previous,3+action));
+        end
         world = zeros(grid_size,grid_size);
         if sum(sum(world)) < (grid_size * grid_size)-1
             snake_location = append_snake(snake_location,previous_snake_element_location);
@@ -103,20 +119,46 @@ while 1
         [fruit_r,fruit_c] = spawn_fruit(world);
         world(fruit_r,fruit_c) = 0.5;
         Q_matrix_fruit = update_q_fruit(Q_matrix_fruit, grid_size, fruit_r, fruit_c);
+    	Global_Q_matrix(Q_matrix_fruit(:,3)',:) = Q_matrix_fruit(:,4:end);
     end
-    if ~fruit_eaten% && ~testing_phase
-        Q_matrix_fruit(ind_in_fruit_previous,3+action) = Q_matrix_fruit(ind_in_fruit_previous,3+action) + learning_rate * (reward_for_moving + discount_factor * max(Q_matrix_fruit(ind_in_fruit_current,4:end)) - Q_matrix_fruit(ind_in_fruit_previous,3+action));
-    end
+    Q_matrix_fruit = update_q_fruit(Q_matrix_fruit, grid_size, fruit_r, fruit_c);
     Global_Q_matrix(Q_matrix_fruit(:,3)',:) = Q_matrix_fruit(:,4:end);
+	
+    if ~fruit_eaten
+    %    Global_Q_matrix(ind_previous,action) = Global_Q_matrix(ind_previous,action) + learning_rate * (reward_for_moving + discount_factor * max(Global_Q_matrix(ind_current,:)) - Global_Q_matrix(ind_previous,action));
+		Q_matrix_fruit(ind_in_fruit_previous,3+action) = Q_matrix_fruit(ind_in_fruit_previous,3+action) + learning_rate * (0 + discount_factor * max(Q_matrix_fruit(ind_in_fruit_current,4:end)) - Q_matrix_fruit(ind_in_fruit_previous,3+action));
+    end
     %Global_Q_matrix = zeros(grid_size * grid_size, 4);
     %visualize_world(world,snake_location,fruit_r,fruit_c,sleep,inf);
     if sum(sum(world==2)) == 1
+		%Global_Q_matrix = zeros(grid_size * grid_size, 4);
         fprintf('No of iteration %d: \n',iterator);
-        fprintf(fid,'Iteration: %d \n',iterator);
-        fprintf(fid,'Snake Length: %d \n\n',size(snake_location,1));
         %explore_exploit_threshold = explore_exploit_threshold - 0.01;
         %size(snake_location,1)
-        iterator = iterator +1;
+		if snake_length > 50
+			fprintf('Length of Snake: %d\n', size(snake_location,1))
+        	visualize_world(world,snake_location,fruit_r,fruit_c,sleep,iterator);
+			pause
+		end
+		if testing_phase && count > 0
+			count = count - 1
+			snake_length_vec = [snake_length_vec; snake_length];
+			fprintf('Length of Snake: %d\n', size(snake_location,1))
+		else
+			snake_length_vec =[];
+		end
+
+		if testing_phase && count == 0
+			%Q_matrix_fruit
+			%collision_matrix
+        	%[[1:(grid_size * grid_size)]', Global_Q_matrix]
+        	fprintf(fid,'Iteration: %d \n',iterator);
+        	fprintf(fid,'Snake Length: %d \n\n', ceil(mean(snake_length_vec)));
+		end
+		
+		if count == 0
+           iterator = iterator +1;
+		end
         world = zeros(grid_size,grid_size);
         [idx,~] = find(snake_location(1,1) == snake_location(2:end,1) & snake_location(1,2) == snake_location(2:end,2));
         if ~isempty(idx)
@@ -130,13 +172,13 @@ while 1
                 ind_in_snake_current = find(collision_matrix(:,3) == ind_current);
                 temp_action = snake_location(k+1,3);
                 current_action = snake_location(k,3);
-                %if ~testing_phase
+                if ~testing_phase
                     if k==1
                         collision_matrix(ind_in_snake_previous,4+temp_action) = collision_matrix(ind_in_snake_previous,4+temp_action) + learning_rate_self * (reward_for_hitting + discount_factor_self * (collision_matrix(ind_in_snake_current,4+current_action)) - collision_matrix(ind_in_snake_previous,4+temp_action));
                     else
                         collision_matrix(ind_in_snake_previous,4+temp_action) = collision_matrix(ind_in_snake_previous,4+temp_action) + learning_rate_self * (discount_factor_self * (collision_matrix(ind_in_snake_current,4+current_action)) - collision_matrix(ind_in_snake_previous,4+temp_action));
                     end
-                %end
+                end
             end
             collision_matrix;
             
@@ -151,6 +193,7 @@ while 1
         [fruit_r, fruit_c] = spawn_fruit(world);
         world(fruit_r,fruit_c) = 0.5;
         Q_matrix_fruit = update_q_fruit(Q_matrix_fruit, grid_size, fruit_r, fruit_c);
+    	Global_Q_matrix(Q_matrix_fruit(:,3)',:) = Global_Q_matrix(Q_matrix_fruit(:,3)',:)  + Q_matrix_fruit(:,4:end);
     end
 
     %visualize_world(world,snake_location,fruit_r,fruit_c,sleep,inf);
@@ -160,7 +203,8 @@ while 1
     else
         extended_length = size(snake_location,1);
     end
-    for d = 1:3
+    %extended_length = size(snake_location,1);
+    for d = 1:6
         for m=5:extended_length
             if d>=m
                 continue;
@@ -179,8 +223,10 @@ while 1
     %pause
     
     if testing_phase
-        visualize_world(world,snake_location,fruit_r,fruit_c,sleep,inf);
+		%Q_matrix_fruit
+		%collision_matrix
         %[[1:(grid_size * grid_size)]', Global_Q_matrix]
+		visualize_world(world,snake_location,fruit_r,fruit_c,sleep,inf);
     end
     
 end
